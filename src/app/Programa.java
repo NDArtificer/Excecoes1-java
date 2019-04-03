67cbcd5a4a06b526cf32a89d8c8054a50f347438
NDArtificer <the.artificer@hotmail.com>
2019-04-03 19:49:13 -0300

    Solução Ruim

--- src/app/Programa.java
+++ src/app/Programa.java
@@ -13,35 +13,40 @@ public class Programa {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		System.out.println("Room number");
+
+		System.out.print("Room number");
 		int roomNumber = sc.nextInt();
-		System.out.println("CheckIn Date (dd/MM/yyyy):");
+		System.out.print("CheckIn Date (dd/MM/yyyy):");
 		Date checkIn = sdf.parse(sc.next());
-		System.out.println("CheckOut Date (dd/MM/yyyy): ");
+		System.out.print("CheckOut Date (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
-		
-		if (! checkOut.after(checkIn)) {
+
+		if (!checkOut.after(checkIn)) {
 			System.out.println("Error reserevation: Check-Out date must be after Check-In date!");
 		} else {
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-			
+
 			System.out.println("");
 			System.out.println("Enter data to update the reserevation: ");
-			System.out.println("CheckIn Date (dd/MM/yyyy):");
+			System.out.print("CheckIn Date (dd/MM/yyyy):");
 			checkIn = sdf.parse(sc.next());
-			System.out.println("CheckOut Date (dd/MM/yyyy): ");
+			System.out.print("CheckOut Date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-			
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
-		}
-		
-		
-		
-		sc.close();
 
+			Date now = new Date();
+
+			if (checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("Error reserevation: Check-Out date must be future date!");
+			} else if (!checkOut.after(checkIn)) {
+				System.out.println("Error reserevation: Check-Out date must be after Check-In date!");
+			} else {
+				reservation.updateDates(checkIn, checkOut);
+				System.out.println("Reservation: " + reservation);
+			}
+		}
+
+		sc.close();
 
 	}
 
